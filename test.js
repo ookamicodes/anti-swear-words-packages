6f60ad3eced5365897e54e983f94bc13290a9188
ookami <zuzu_zuzu2623@hotmail.com>
2020-10-05 22:28:38 +0200

    test V

---
+++ test.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+client.login('NzYxOTM5NjI2ODkyOTg0MzIx.X3h5kw.GjFrovrqwUwWIIADLy_-029rzWY');
